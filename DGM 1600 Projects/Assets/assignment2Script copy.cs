d0f24e497784be861961540b4670fb9c45200984
Isaac Ericksen <ericksen34@gmail.com>
2016-01-12 19:06:24 -0700

    DGM 1600 Assignment 2 (var & functions)

---
+++ DGM 1600 Projects/Assets/assignment2Script copy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class assignment2Script : MonoBehaviour {
+
+//Debug.log allows me to get the value of any variable in the script or game.
+
+//Start is called when the object this script is attached to enters abstract scene.
+
+
+	int aNumber = 5;
+
+	void Start(){
+		SubtractByThree(aNumber);
+		aNumber = SubtractByThree(aNumber);
+		Debug.Log (aNumber);
+	}
+	int SubtractByThree(int bNumber){
+			int answer;
+			answer = bNumber - 3;
+
+			return answer;
+		}
+}
