49dd2376bf79a458c284f568ca0ceeaef6ae16d9
Isaac Ericksen <ericksen34@gmail.com>
2016-02-18 18:30:02 -0700

    DGM 1600 - If statement assignment

---
+++ ifstatements.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ifstatements : MonoBehaviour
+{
+	float yourScore = 10;
+	float averageScore = 7;
+	float lowScore = 5;
+	
+	// Use this for initialization
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			popQuiz();
+		yourScore -= Time.deltaTime * 1;
+	}
+		
+	// Update is called once per frame
+	void popQuiz ()
+	{
+
+		if (yourScore > averageScore){ 
+
+			print ("You did amazing!");
+		}
+		else if (yourScore < lowScore){
+			print ("You failed");
+		}
+		else {
+			print ("Good Job.");
+		}
+	}
+}
